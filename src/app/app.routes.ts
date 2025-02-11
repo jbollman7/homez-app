c98b2ddaf27fb7693a71185269a2ca84f41adbf4
Joe Bollman <joseph.bollman@ineight.com>
2025-02-11 07:11:25 -0700

    Added paramtertized routes
    * removed home selector from app.component, replaced with router-outlet
    * Added two routes to route component, default Home component, and
      Detail Component.
    * In Details.componet we make a class property and get the paramtertized
      value from the route, cast to a Number type.  In details template, we
      simply output our value.
    * Router link added to housing-location component - New link that goes
      to detail page of the specific listing.  This is where the url gets
      the param for the url!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { DetailsComponent } from './details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    component: HomeComponent,
+    path: '',
+    title: 'Homey Page'
+  },
+  {
+    component: DetailsComponent,
+    path: 'details/:id',
+    title: 'Details Page'
+  }
+
+];
